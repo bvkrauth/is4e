8971dd4928fb356bddb2a7f69e5a8e88871426b5
Brian Krauth <bkrauth@sfu.ca>
2022-12-14 16:18:55 -0800

    Add variables for color scheme
    Signed-off-by: Brian Krauth <bkrauth@sfu.ca>

--- drawgraphs.R
+++ drawgraphs.R
@@ -2,6 +2,11 @@
 library(tidyverse)
 library(ggplot2)
 
+# Set color scheme
+
+AstroBlue <- "#002D62"
+AstroOrange <- "#EB6E1F"
+
 # Picture 1: Supply and demand graph to be used as the icon
 # in the "economics" box
 supplydemand <- tibble(Q=1:20)
@@ -11,28 +16,28 @@ supplydemand$PD <- 21 - supplydemand$Q
 sd_plot <- ggplot(data=supplydemand,
               mapping=aes(x=Q)) + 
   geom_line(mapping=aes(y=PS),
-            colour="#EB6E1F",
-            size=3) + 
+            colour=AstroOrange,
+            linewidth=3) +
   geom_line(mapping=aes(y=PD),
-            colour="#002D62",
-            size=3) +
+            colour=AstroBlue,
+            linewidth=3) +
   labs(x="Q",
        y="P") +
   annotate("text",
            x=20,
            y=19,
            label="S",
-           col="#EB6E1F") +
+           col=AstroOrange) +
   annotate("text",
            x=20,
            y=2,
            label="D",
-           col="#002D62") +
+           col=AstroBlue) +
   theme_classic() +
   theme(axis.text.x=element_blank(),
         axis.text.y=element_blank(),
         axis.ticks=element_blank(),
-        axis.line=element_line(size=2,
+        axis.line=element_line(linewidth=2,
                                arrow=arrow(type='open',
                                            length = unit(10,'pt'))))
 show(sd_plot)
@@ -53,25 +58,25 @@ EmpData <- read_csv("sampledata/EmploymentData.csv") %>%
 emp_hist <- ggplot(data = EmpData,
        aes(x = UnempPct)) + 
   geom_histogram(binwidth = 0.5,
-                 fill = "#002D62") +
+                 fill = AstroBlue) +
   geom_density() +
   annotate("text",
            x=11,
            y=90,
            label="Introductory",
-           col="#EB6E1F",
+           col=AstroOrange,
            size=15) +
   annotate("text",
            x=11,
            y=75,
            label="Statistics for",
-           col="#EB6E1F",
+           col=AstroOrange,
            size=15) +
   annotate("text",
            x=11,
            y=60,
            label="Economics",
-           col="#EB6E1F",
+           col=AstroOrange,
            size=15) +
   labs(title = "Unemployment rate",
        subtitle = paste("January 1976 - January 2021 (",
@@ -89,5 +94,3 @@ show(emp_hist)
 png(file="logo.png",bg="transparent")
 show(emp_hist)
 dev.off()
-
-
